b0356132a0bb4c6dbf963aff1ddf44cc24bb7a6b
Srilakshmi SV <srilakshmi2011.v@gmail.com>
2019-01-11 23:51:39 +0530

    FirebaseTask2 first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.deleteOldUser = functions.database.ref('/users/').onWrite((change) => {
+  const parentRef = change.after.ref.parent; // reference to the parent
+
+  
+  const olduserQuery=parentRef.orderByChild('age').limitToLast(1);
+  
+  return olduserQuery.once('value').then((snapshot) => {
+    
+    const updates = {};
+    snapshot.forEach(child => {
+      updates[child.key] = null;
+    });
+    
+    return parentRef.update(updates);
+  });
+});
